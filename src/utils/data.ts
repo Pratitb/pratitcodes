2ef1363d5e9d124f0bb5ae0f77b2e1553d04211e
Pratit Bangdiwala <pratit.bangdiwala7@gmail.com>
2025-12-29 09:27:46 +0530

    enha: reduced tech tabs

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -6,7 +6,7 @@ import {
 	CustDash,
 	Faq,
 	Food,
-	Fylo,
+	// Fylo,
 	Tip,
 	Profile,
 	Social,
@@ -17,66 +17,65 @@ import type { ProjectsType } from './types'
 
 export const projectTypes = [
 	'react',
-	'typescript',
-	'tailwindCSS',
+	// 'typeScript',
+	// 'tailwindCSS',
 	'components',
+	'API/Fetch',
 	'javaScript',
-	'SCSS',
-	'design to code',
 ]
 
 export const projects: ProjectsType[] = [
 	{
 		name: 'crypto dash',
-		tech: ['components', 'react', 'typescript'],
+		tech: ['API/Fetch'],
 		coverBg: Crypto,
 		url: 'https://pratitb.github.io/crypto-dash/',
 	},
 	{
 		name: 'job list filtering',
-		tech: ['components', 'react', 'typescript'],
+		tech: ['components'],
 		coverBg: Jobs,
 		url: 'https://pratitb.github.io/job-list-filtering/',
 	},
 	{
 		name: 'social media dashboard',
-		tech: ['components', 'tailwindCSS'],
+		tech: ['components'],
 		coverBg: Social,
 		url: 'https://pratitb.github.io/social-media/',
 	},
 	{
 		name: 'profile card',
-		tech: ['components', 'tailwindCSS'],
+		tech: ['components'],
 		coverBg: Profile,
 		url: 'https://pratitb.github.io/profile-card/',
 	},
 	{
 		name: 'food item card',
-		tech: ['components', 'tailwindCSS'],
+		tech: ['components'],
 		coverBg: Food,
 		url: 'https://pratitb.github.io/food-item-card/',
 	},
 	{
 		name: 'Automotive Cards',
-		tech: ['components', 'tailwindCSS'],
+		tech: ['components'],
 		coverBg: Car,
 		url: 'https://pratitb.github.io/car-type-card/',
 	},
 	{
 		name: 'FAQs',
-		tech: ['components', 'tailwindCSS'],
+		tech: ['components'],
 		coverBg: Faq,
 		url: 'https://pratitb.github.io/faqs/',
 	},
-	{
+	/* {
 		name: 'fylo',
-		tech: ['tailwindCSS', 'design to code'],
+		tech: ['tailwindCSS'],
 		coverBg: Fylo,
 		url: 'https://pratitb.github.io/fylo/',
-	},
+	}, */
 	{
 		name: 'customer dashboard',
-		tech: ['react', 'typescript', 'SCSS'],
+		tech: ['API/Fetch', 'react', 'SCSS'],
 		coverBg: CustDash,
 		url: 'https://pratitb.github.io/customer-dashboard/',
 	},
@@ -88,7 +87,7 @@ export const projects: ProjectsType[] = [
 	}, */
 	{
 		name: 'currency converter',
-		tech: ['react'],
+		tech: ['API/Fetch', 'react'],
 		coverBg: Currency,
 		url: 'https://currencify.in/',
 	},
